7b1f5078841a9d8f15baf155569e421ce2168428
unknown <quentinprs62@gmail.com>
2021-02-21 22:18:34 +0100

    add calamities in the file json

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,18 +1,22 @@
 module com.esgi {
-    requires javafx.base;
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires com.fasterxml.jackson.core;
-    requires com.fasterxml.jackson.databind;
-    requires com.fasterxml.jackson.annotation;
-    requires javafx.media;
-    opens com.esgi to javafx.fxml;
-    exports com.esgi;
-    exports com.esgi.models;
-    exports com.esgi.models.Calamities;
+        requires javafx.base;
+        requires javafx.controls;
+        requires javafx.fxml;
+        requires com.fasterxml.jackson.core;
+        requires com.fasterxml.jackson.databind;
+        requires com.fasterxml.jackson.annotation;
+        requires javafx.media;
+        requires junit;
+        opens com.esgi to javafx.fxml;
+        exports com.esgi;
+        exports com.esgi.models;
+        exports com.esgi.models.Calamities;
 
-    opens com.esgi.models;
-    opens com.esgi.models.Calamities;
-    opens com.esgi.sound;
-    opens com.esgi.controllers;
+
+        opens com.esgi.models;
+        opens com.esgi.models.Calamities;
+        opens com.esgi.sound;
+        opens com.esgi.controllers;
+
+
 }
